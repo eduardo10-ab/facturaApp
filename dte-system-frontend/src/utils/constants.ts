caad72b0d4a074152c8ce6e8bb35b21082890394
Eduardo <eduardofuentes2610@gmail.com>
2025-10-17 09:54:36 -0600

    se han agregado los formularios restantes y se actualizo la lista de dpts y municipios

--- dte-system-frontend/src/utils/constants.ts
+++ dte-system-frontend/src/utils/constants.ts
@@ -1,72 +1,118 @@
 // Migración de dte.js departamentosYMunicipios
 export const DEPARTAMENTOS_MUNICIPIOS = {
-  "01": { 
-    nombre: "Ahuachapán", 
-    municipios: { 
-      "01":"Ahuachapán", "02":"Apaneca", "03":"Atiquizaya", 
-      "04":"Concepción de Ataco", "05":"El Refugio", "06":"Guaymango", 
-      "07":"Jujutla", "08":"San Francisco Menéndez", "09":"San Lorenzo", 
-      "10":"San Pedro Puxtla", "11":"Tacuba", "12":"Turín" 
-    } 
+  "01": {
+    nombre: "Ahuachapán",
+    municipios: {
+      "01": "Ahuachapán Norte",
+      "02": "Ahuachapán Centro",
+      "03": "Ahuachapán Sur"
+    }
   },
-  "02": { 
-    nombre: "Cabañas", 
-    municipios: { 
-      "01":"Cinquera", "02":"Dolores", "03":"Guacotecti", 
-      "04":"Ilobasco", "05":"Jutiapa", "06":"San Isidro", 
-      "07":"Sensuntepeque", "08":"Tejutepeque", "09":"Victoria" 
-    } 
+  "02": {
+    nombre: "Santa Ana",
+    municipios: {
+      "01": "Santa Ana Norte",
+      "02": "Santa Ana Centro",
+      "03": "Santa Ana Este",
+      "04": "Santa Ana Oeste"
+    }
   },
-  "03": { 
-    nombre: "Chalatenango", 
-    municipios: { 
-      "01":"Agua Caliente", "02":"Arcatao", "03":"Azacualpa", 
-      "04":"Citalá", "05":"Comalapa", "06":"Concepción Quezaltepeque", 
-      "07":"Chalatenango", "08":"Dulce Nombre de María", "09":"El Carrizal", 
-      "10":"El Paraíso", "11":"La Laguna", "12":"La Palma", 
-      "13":"La Reina", "14":"Las Vueltas", "15":"Nombre de Jesús", 
-      "16":"Nueva Concepción", "17":"Nueva Trinidad", "18":"Ojos de Agua", 
-      "19":"Potonico", "20":"San Antonio de la Cruz", "21":"San Antonio Los Ranchos", 
-      "22":"San Fernando", "23":"San Francisco Lempa", "24":"San Francisco Morazán", 
-      "25":"San Ignacio", "26":"San Isidro Labrador", "27":"San Luis del Carmen", 
-      "28":"San Miguel de Mercedes", "29":"San Rafael", "30":"Santa Rita", 
-      "31":"Tejutla" 
-    } 
+  "03": {
+    nombre: "Sonsonate",
+    municipios: {
+      "01": "Sonsonate Norte",
+      "02": "Sonsonate Centro",
+      "03": "Sonsonate Este",
+      "04": "Sonsonate Oeste"
+    }
   },
-  "04": { 
-    nombre: "Cuscatlán", 
-    municipios: { 
-      "01":"Candelaria", "02":"Cojutepeque", "03":"El Carmen", 
-      "04":"El Rosario", "05":"Monte San Juan", "06":"Oratorio de Concepción", 
-      "07":"San Bartolomé Perulapía", "08":"San Cristóbal", "09":"San José Guayabal", 
-      "10":"San Pedro Perulapán", "11":"San Rafael Cedros", "12":"San Ramón", 
-      "13":"Santa Cruz Analquito", "14":"Santa Cruz Michapa", "15":"Suchitoto", 
-      "16":"Tenancingo" 
-    } 
+  "04": {
+    nombre: "Chalatenango",
+    municipios: {
+      "01": "Chalatenango Norte",
+      "02": "Chalatenango Centro",
+      "03": "Chalatenango Sur"
+    }
   },
-  "05": { 
-    nombre: "La Libertad", 
-    municipios: { 
-      "01":"Antiguo Cuscatlán", "02":"Ciudad Arce", "03":"Colón", 
-      "04":"Comasagua", "05":"Chiltiupán", "06":"Huizúcar", 
-      "07":"Jayaque", "08":"Jicalapa", "09":"La Libertad", 
-      "10":"Santa Tecla", "11":"Nuevo Cuscatlán", "12":"San Juan Opico", 
-      "13":"Quezaltepeque", "14":"Sacacoyo", "15":"San José Villanueva", 
-      "16":"San Matías", "17":"San Pablo Tacachico", "18":"Tamanique", 
-      "19":"Talnique", "20":"Teotepeque", "21":"Tepecoyo", 
-      "22":"Zaragoza" 
-    } 
+  "05": {
+    nombre: "La Libertad",
+    municipios: {
+      "01": "La Libertad Norte",
+      "02": "La Libertad Centro",
+      "03": "La Libertad Oeste",
+      "04": "La Libertad Este",
+      "05": "La Libertad Costa",
+      "06": "La Libertad Sur"
+    }
   },
-  "10": { 
-    nombre: "San Salvador", 
-    municipios: { 
-      "01":"Aguilares", "02":"Apopa", "03":"Ayutuxtepeque", 
-      "04":"Cuscatancingo", "05":"El Paisnal", "06":"Guazapa", 
-      "07":"Ilopango", "08":"Mejicanos", "09":"Nejapa", 
-      "10":"Panchimalco", "11":"Rosario de Mora", "12":"San Marcos", 
-      "13":"San Martín", "14":"San Salvador", "15":"Santiago Texacuangos", 
-      "16":"Santo Tomás", "17":"Soyapango", "18":"Tonacatepeque" 
-    } 
+  "06": {
+    nombre: "San Salvador",
+    municipios: {
+      "01": "San Salvador Norte",
+      "02": "San Salvador Oeste",
+      "03": "San Salvador Este",
+      "04": "San Salvador Centro",
+      "05": "San Salvador Sur"
+    }
+  },
+  "07": {
+    nombre: "Cuscatlán",
+    municipios: {
+      "01": "Cuscatlán Norte",
+      "02": "Cuscatlán Sur"
+    }
+  },
+  "08": {
+    nombre: "La Paz",
+    municipios: {
+      "01": "La Paz Oeste",
+      "02": "La Paz Centro",
+      "03": "La Paz Este"
+    }
+  },
+  "09": {
+    nombre: "Cabañas",
+    municipios: {
+      "01": "Cabañas Este",
+      "02": "Cabañas Oeste"
+    }
+  },
+  "10": {
+    nombre: "San Vicente",
+    municipios: {
+      "01": "San Vicente Norte",
+      "02": "San Vicente Sur"
+    }
+  },
+  "11": {
+    nombre: "Usulután",
+    municipios: {
+      "01": "Usulután Norte",
+      "02": "Usulután Este",
+      "03": "Usulután Oeste"
+    }
+  },
+  "12": {
+    nombre: "San Miguel",
+    municipios: {
+      "01": "San Miguel Norte",
+      "02": "San Miguel Centro",
+      "03": "San Miguel Oeste"
+    }
+  },
+  "13": {
+    nombre: "Morazán",
+    municipios: {
+      "01": "Morazán Norte",
+      "02": "Morazán Sur"
+    }
+  },
+  "14": {
+    nombre: "La Unión",
+    municipios: {
+      "01": "La Unión Norte",
+      "02": "La Unión Sur"
+    }
   }
   // Agregar resto de departamentos según necesidad
 } as const;
